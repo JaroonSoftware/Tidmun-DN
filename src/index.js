6c24fdee5bb7ab1f5d22232a42a22335a966e20f
Chayapat <jaroonsoft@hotmail.com>
2024-08-25 23:55:37 +0700

    add dn master

--- src/index.js
+++ src/index.js
@@ -1,21 +1,65 @@
 
 
-$(document).mousemove(function (event) {
-	$("#tx_empcode").focus();
-});
+// $(document).mousemove(function (event) {
+// 	$("#tx_barcode").focus();
+// });
 
-function createBrowserWindow() {
+function ChangeSO() {
 
 	const ipc = require("electron").ipcRenderer;
 	ipc.send('message:loginShow');
 
 }
-function createBrowserWindow2() {
+function checkBarcode() {
 
 	const ipc = require("electron").ipcRenderer;
 	ipc.send('message:loginShow2');
 
 }
+
+function startScan() {
+
+	if ($('#socode').val() !== '') {
+		$('#tx_barcode').attr("placeholder", "สแกนบาร์โค้ดที่นี่");
+		$('#tx_barcode').prop( "disabled", false );
+		$("#tx_barcode").focus();
+		$("#btnStartScan").hide();
+		$("#btnCreate").show();		
+
+		$.post("https://tidmunzbuffet.com/api_app/dn/add_dnmaster.php", { socode: $('#socode').val() ,cuscode:$('#cuscode').val() }, function (response2) {
+
+			let r2 = JSON.parse(response2)
+			$('#dncode').val(r2.dncode)
+		}).fail(function (error) {
+
+			$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
+		});		
+		
+	}
+	else {
+	  Swal.fire({
+		title: "<strong>กรุณาเลือกใบขายสินค้าก่อน</strong>",
+		icon: "error",
+	  });
+	}
+  }
+  
+  function createDN() {
+
+	if ($('#socode').val() !== '') {
+		$('#tx_barcode').attr("placeholder", "สแกนบาร์โค้ดที่นี่");
+		$('#tx_barcode').prop( "disabled", false );
+		$("#tx_barcode").focus();
+		$("#btnStartScan").hide();
+		$("#btnCreate").show();				
+	}
+	else {
+	  Swal.fire({
+		title: "<strong>กรุณาเลือกใบขายสินค้าก่อน</strong>",
+		icon: "error",
+	  });
+	}
+  }
 
 var count = 0;
 
@@ -37,41 +81,52 @@ function addText(event) {
 	document.getElementById("tx_unitweigt").value = parseFloat(event.substring(1, 8)).toFixed(2);
 }
 
-var inputempcode = document.getElementById("tx_empcode");
+var inputbarcode = document.getElementById("tx_barcode");
 var inputweight = document.getElementById("tx_unitweigt");
+var inputsocode = document.getElementById("socode");
 var str;
 
-inputempcode.addEventListener("keypress", function (event) {
+inputbarcode.addEventListener("keypress", function (event) {
 	if (event.key === "Enter") {
 
+		// alert(inputsocode)
 		if (inputweight.value > 0) {
-
-			let data = response;
 			// console.log(data['data'][0]['empcode'])
-			$.post("https://tidmunzbuffet.com/api_app/barcode/add_barcode.php", { empcode: data['data'][0]['empcode'], unit_weight: inputweight.value, product_id: $("#selproduct").val(), create_date: data['data'][0]['create_date'] }, function (response2) {
+
+			$.post("https://tidmunzbuffet.com/api_app/dn/add_dnmaster.php", { socode: inputsocode.value ,cuscode:$('#cuscode').val() }, function (response2) {
 
 				let r2 = JSON.parse(response2)
-
-				if (r2.status === 1) {
-					count++;
-					str = '';
-					str += '<tr id="' + count + '"><td>' + data['data'][0]['display_date'] + '</td><td>' + data['data'][0]['empcode'] + '</td><td>' + data['data'][0]['firstname'] + ' ' + data['data'][0]['lastname'] + '</td><td>' + inputweight.value + '</td><td>0</td><td><button class="btn btn-secondary" onclick="PrintBarcode(' + r2.id + ');"> Edit</button></td>';
-					str += '</tr>';
-
-					$("#tbmain").prepend(str);
-					const elmnt = document.querySelector("table tr:last-child");
-					elmnt.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-					document.getElementById('txtresult').innerHTML = 'บันทึกข้อมูลรหัส ' + data['data'][0]['empcode'] + ' สำเร็จ '
-					document.getElementById('txtresult').style.color = "#4DBE05";
-				}
-				else {
-					document.getElementById('txtresult').innerHTML = r2.message
-					document.getElementById('txtresult').style.color = "red";
-				}
+				$('#dncode').val(r2.dncode)
 			}).fail(function (error) {
 
 				$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
 			});
+
+
+			// $.post("https://tidmunzbuffet.com/api_app/dn/add_dndetail.php", { empcode: data['data'][0]['empcode'], unit_weight: inputweight.value, product_id: $("#selproduct").val(), create_date: data['data'][0]['create_date'] }, function (response2) {
+
+			// 	let r2 = JSON.parse(response2)
+
+			// 	if (r2.status === 1) {
+			// 		count++;
+			// 		str = '';
+			// 		str += '<tr id="' + count + '"><td>' + data['data'][0]['display_date'] + '</td><td>' + data['data'][0]['empcode'] + '</td><td>' + data['data'][0]['firstname'] + ' ' + data['data'][0]['lastname'] + '</td><td>' + inputweight.value + '</td><td>0</td><td><button class="btn btn-secondary" onclick="PrintBarcode(' + r2.id + ');"> Edit</button></td>';
+			// 		str += '</tr>';
+
+			// 		$("#tbmain").prepend(str);
+			// 		const elmnt = document.querySelector("table tr:last-child");
+			// 		elmnt.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+			// 		document.getElementById('txtresult').innerHTML = 'บันทึกข้อมูลรหัส ' + data['data'][0]['empcode'] + ' สำเร็จ '
+			// 		document.getElementById('txtresult').style.color = "#4DBE05";
+			// 	}
+			// 	else {
+			// 		document.getElementById('txtresult').innerHTML = r2.message
+			// 		document.getElementById('txtresult').style.color = "red";
+			// 	}
+			// }).fail(function (error) {
+
+			// 	$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
+			// });
 
 
 
@@ -81,8 +136,8 @@ inputempcode.addEventListener("keypress", function (event) {
 			document.getElementById('txtresult').style.color = "red";
 
 		}
-		inputempcode.value = null;
-		inputempcode.click();
+		inputbarcode.value = null;
+		inputbarcode.click();
 		event.preventDefault();
 	}
 });
